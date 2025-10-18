25204b312b211f45b05edbd08018de3a569bfff5
f2025-0331-glitch <f2025-0331@bnu.edu.pk>
2025-10-18 13:23:14 +0500

    commit 1

---
+++ Lab 2.cpp
@@ -0,0 +1,12 @@
+// Created by Dabeer on 18/10/2025.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int Temp;
+    cout << "Enter Temperature: ";
+    cin>> Temp;
+    Temp = (Temp * 9 / 5) + 32;
+    cout << "Temperature in Ferenheit: " << Temp;
+return 0;
+}
