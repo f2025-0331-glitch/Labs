8793161f5f412756fa56a7fb69b82ee8553d35ee
f2025-0331-glitch <f2025-0331@bnu.edu.pk>
2025-10-31 11:35:58 +0500

    commit 4

--- Lab 5.cpp
+++ Lab 5.cpp
@@ -1,14 +1,14 @@
 // Created by Dabeer on 24/10/2025.
-
+//Question 1
 #include <iostream>
 using namespace std;
 int main() {
     int n;
     cout << "Enter the Size of the Pyramid: ";
     cin >> n;
-    for (int i = 1; i <= n; ++i) {
-        for (int s = 0; s < n - i; ++s) cout << ' ';
-        for (int j = 0; j < 2 * i - 1; ++j) cout << '*';
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < n - i; j++){ cout << " " ;}
+        for (int k = 0; k < i ; k++){ cout << "* ";}
         cout << '\n';
     }
     return 0;
