09fc73d2b898cbfe34fa08f0bca2409b3cbe83a4
f2025-0331-glitch <f2025-0331@bnu.edu.pk>
2025-10-31 12:29:51 +0500

    commit 6

---
+++ Lab 6(Q1b).cpp
@@ -0,0 +1,19 @@
+// Created by Dabeer on 31/10/2025.
+
+#include <iostream>
+using namespace std;
+int main() {
+    string password;
+    while (true) {
+        cout << "Enter password: ";
+        cin >> password;
+        if (password == "Python123") {
+            cout << "Correct Password" << endl;
+            break;
+        }
+        else {
+            cout << "Wrong Password" << endl;
+        }
+    }
+    return 0;
+}
