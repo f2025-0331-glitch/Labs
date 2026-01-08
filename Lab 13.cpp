0adfe33107b5f51f3808cda641c6846aef33fe78
dabeer <f2025-0331@bnu.edu.pk>
2026-01-09 01:45:28 +0500

    commit 16

---
+++ Lab 13.cpp
@@ -0,0 +1,30 @@
+// Created by Mirza Dabeer Mehmood on 09/01/2026.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    int sum = 0;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    vector<vector<int>> matrix(rows, vector<int>(cols));
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+            sum += matrix[i][j];
+        }
+    }
+
+    cout << "Sum of all elements = " << sum << endl;
+
+    return 0;
+}
