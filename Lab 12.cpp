246bd7ce5a474addbb72875aba1a2e0517dced85
dabeer <f2025-0331@bnu.edu.pk>
2026-01-09 01:44:26 +0500

    commit 15

---
+++ Lab 12.cpp
@@ -0,0 +1,26 @@
+// Created by Mirza Dabeer Mehmood on 09/01/2026.
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sumAndAverage(const vector<int>& v) {
+    int sum = 0;
+
+    for (int x : v) {
+        sum += x;
+    }
+
+    double average = static_cast<double>(sum) / v.size();
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+}
+
+int main() {
+    vector<int> numbers = {10, 20, 30, 40, 50};
+
+    sumAndAverage(numbers);
+
+    return 0;
+}
