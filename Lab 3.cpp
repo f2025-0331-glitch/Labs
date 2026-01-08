d1996763c96c401292d2646e6464b506bc1f40b7
dabeer <f2025-0331@bnu.edu.pk>
2026-01-09 01:42:25 +0500

    commit 13

---
+++ Lab 3.cpp
@@ -0,0 +1,27 @@
+// Created by Mirza Dabeer Mehmood on 09/01/2026.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int secret = 7;
+    int guess;
+
+    cout << "Guess the secret number: ";
+    cin >> guess;
+
+    while (guess != secret) {
+        if (guess > secret) {
+            cout << "Too high\n";
+        } else {
+            cout << "Too low\n";
+        }
+
+        cout << "Guess again: ";
+        cin >> guess;
+    }
+
+    cout << "Congratulations! You guessed the correct number.\n";
+
+    return 0;
+}
